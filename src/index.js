4a9dfc9b4575a9ce4573829e910224c5bd2e044c
Allan Gabriel <allan.gabriel@protonmail.com>
2020-09-27 14:44:22 -0300

    add bot call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 
 const Discord       = require("discord.js"),
+      fs            = require("fs"),
       { prefix }    = require("./config.json");
 
+//client setup
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
+//require command files
+const commandFiles = fs.readdirSync(__dirname + "/commands")
+    .filter(command => command.endsWith(".js"));
+
+//requires and add commands to client 
+commandFiles.forEach(file => {
+    const command = require(`${__dirname}/commands/${file}`);
+    client.commands.set(command.name, command);
+});
+
+//handle messages
 client.on("message", msg => {
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-    msg.channel.send("Olá :)");
+    const options = msg.content.slice(prefix.length).trim().split(" ");
+    const command = options.shift().toLowerCase();
+
+    if(!command.length) {
+        client.commands.get("ajuda").execute(msg, options);
+    } else {
+        client.commands.get(command).execute(msg, options);
+    }
 });
 
 client.on("ready", () => 
