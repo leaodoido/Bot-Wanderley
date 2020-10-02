559f1b15fe46bafd9d099b32510af44f7a416207
Allan Gabriel <allan.gabriel@protonmail.com>
2020-10-02 18:41:53 -0300

    add reclamacao command

---
+++ src/commands/reclamacao.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name:"reclamacao",
+    description: "registra uma reclamação",
+    execute(msg) {
+        msg.reply(" sua reclamação foi encaminhada com sucesso ao nosso departamento do FODASE '-'");
+    }
+}
