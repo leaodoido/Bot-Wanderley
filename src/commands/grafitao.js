5f74c279411b7f761d07a7b659ad09b72a2b7130
Ribeiro <joaorg1@hotmail.com>
2021-02-12 20:56:02 -0300

    Adciona grafitão e resolve possiveis bugs no comando (lansa)

--- src/commands/grafitao.js
+++ src/commands/grafitao.js
@@ -1,28 +1,26 @@
-const path = require("path");
+const ytdl = require("ytdl-core");
+const { grafith } = require("../resources/urls.json");
 
 module.exports = {
-    name:"grafitao",
-    description: "Toca uma musica aleatoria de graffith",
-    async execute(msg, args) {
-		const { voice } = msg.member;
+  name: "grafitao",
+  description: "Toca uma musica aleatoria de graffith",
+  async execute(msg, args) {
+    const { voice } = msg.member;
 
-		if (!voice.channelID){
-			msg.reply("Ei galado, tu tem que estar num voice ta ligado nao?? '-'");
-			return;
-		}
-
-		const song = Math.floor((Math.random() * 2) + 1);
-
-
-		try {
-			const connection = await voice.channel.join();
-		
-			const dispatcher = connection.play(path.join(__dirname, "../resources/" + song + ".mp3"));
-
-			dispatcher.on("finish", () => (voice.channel.leave()));
-		} catch (e) {
-			msg.reply("Ei galado, deu errado visse?");
-		}
-
+    if (!voice.channelID) {
+      msg.reply("Ei galado, tu tem que estar num voice ta ligado nao?? '-'");
+      return;
     }
-}
+    const song = Math.floor(Math.random() * grafith.length);
+
+    try {
+      const connection = await voice.channel.join();
+
+      const dispatcher = connection.play(ytdl(grafith[song]));
+
+      dispatcher.on("finish", () => voice.channel.leave());
+    } catch (e) {
+      msg.reply("Ei galado, deu errado visse?");
+    }
+  },
+};
--- src/commands/lansa.js
+++ src/commands/lansa.js
@@ -1,36 +1,32 @@
-const https = require("https");
-const translations = require("./translations.json");
+const fetch = require("node-fetch");
+const translations = require("../resources/translations.json");
 
 module.exports = {
-    name:"lansa",
-    description: "Te retorna umas paradas massa (Use a sua imaginação)",
-    execute(msg, args) {
-        //checks if the current channel is nsfw
-        if(!msg.channel.nsfw) {
-            msg.reply(" aqui nao seu degenerado!");
-            return;
-        }
-        
-        //mount url
-        let URL = "";
-
-        if(translations[args[0]])
-            URL += `https://nekos.life/api/v2/img/${translations[args[0]]}`;
-        else
-            URL += `https://nekos.life/api/v2/img/${args[0]}`;
-
-        //request data
-        https.get(URL, (res) => {
-            let reqData = "";
-            res
-                .on("data", data => {
-                    reqData += data;
-                })
-                .on("end", () => {
-                    const parsedData = JSON.parse(reqData);
-                    msg.channel.send(parsedData.url);
-                })
-                .on("error", (err) => console.error(err));
-        });
+  name: "lansa",
+  description: "Te retorna umas paradas massa (Use a sua imaginação)",
+  async execute(msg, args) {
+    //checks if the current channel is nsfw
+    if (!msg.channel.nsfw) {
+      msg.reply(" aqui nao seu degenerado!");
+      return;
     }
-}
+
+    //mount url
+    let URL = "";
+
+    if (translations[args[0]])
+      URL += `https://nekos.life/api/v2/img/${translations[args[0]]}`;
+    else URL += `https://nekos.life/api/v2/img/${args[0]}`;
+
+    //request data
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+
+        await msg.channel.send(data.url);
+    } catch (error) {
+      msg.reply(" Ei boy, tem esse nao '-' (tente calango)");
+    }
+  },
+};
+
